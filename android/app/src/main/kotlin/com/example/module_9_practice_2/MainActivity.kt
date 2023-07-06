2f9bb86c331a44e7791db966b86d1fe3dfb56e34
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-06 14:14:53 +0600

    env setup done

---
+++ android/app/src/main/kotlin/com/example/module_9_practice_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_9_practice_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Module 9 Practice 2",
+      home: HomeScreen(),
+    );
+  }
+}
+
